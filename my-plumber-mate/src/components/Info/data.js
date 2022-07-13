67a2e0ccbb8d377fd25a014e688577e9b431f05b
Milad Ezame <milad.ezame@gmail.com>
2022-07-13 15:04:45 +1000

    added info section, styling is not applying, need to fix it.

---
+++ my-plumber-mate/src/components/Info/data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Plumber',
+  headLine: 'i am a plumber',
+  description: 'i steal ur money',
+  buttonLabel: 'Discover',
+  imgStart: false,
+  img: require('../../images/svg-1.svg').default,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+}
---
+++ my-plumber-mate/src/components/Info/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { useState } from 'react'
+import { Button } from '../Sidebar/buttonElements'
+
+import {
+  ImgWrap,
+  Img,
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  ArrowForward,
+  ArrowRight,
+  BtnWrap
+} from './InfoElements'
+
+const Info = ({lightBg, id, ImgStart, topLine, lightText, darkText, description, buttonLabel, headLine, img, alt, primary, dark1, dark2}) => {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
+
+  return (
+    <>
+      <InfoContainer lightBg={lightBg} id={id}>
+        <InfoWrapper>
+          <InfoRow imgStart={ImgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headLine}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
+                <BtnWrap>
+                  <Button to='home>'
+                  onMouseEnter={onHover}
+                  onMouseLeave={onHover}
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  primary={primary ? 1 : 0}
+                  dark1={dark1 ? 1 : 0}
+                  dark2={dark2 ? 1 : 0}
+                  >{buttonLabel} {hover ? <ArrowForward/> : <ArrowRight/>}</Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt}/>
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  )
+}
+
+export default Info
